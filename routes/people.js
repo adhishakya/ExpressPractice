94c9636924956b64696375d87c3c1c365db64c12
Adhish Shakya <shakyaadhish@gmail.com>
2023-03-19 21:18:22 +0545

    Alternate method of routing

--- routes/people.js
+++ routes/people.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const { getPeople, postPerson, postPersonPostman, putPerson, deletePerson } = require('../controllers/people')
 
-router.get('/', getPeople);
+// router.get('/', getPeople);
+// router.post('/', postPerson);
+// router.post('/postman', postPersonPostman);
+// router.put('/:id', putPerson);
+// router.delete('/:id', deletePerson);
 
-router.post('/', postPerson);
-
-router.post('/postman', postPersonPostman);
-
-router.put('/:id', putPerson);
-
-router.delete('/:id', deletePerson);
+router.route('/').get(getPeople).post(postPerson);
+router.route('/postman').post(postPersonPostman);
+router.route('/:id').put(putPerson).delete(deletePerson);
 
 module.exports = router;
