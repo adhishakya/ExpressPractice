19359edbe5c7a5f4e31c2d9c3f1a9762b24e66f1
Adhish Shakya <shakyaadhish@gmail.com>
2023-03-19 21:14:17 +0545

    Route for people

--- routes/people.js
+++ routes/people.js
@@ -1,54 +1,16 @@
 const express = require('express');
 const router = express.Router();
-let { people } = require('../data');
 
+const { getPeople, postPerson, postPersonPostman, putPerson, deletePerson } = require('../controllers/people')
 
-router.get('/', (req, res) => {
-    res.status(200).json({ success: true, data: people });
-});
+router.get('/', getPeople);
 
-router.post('/', (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res.status(400).json({ success: false, message: "Please provide a name" });
-    }
-    res.status(201).json({ success: true, person: name });
-});
+router.post('/', postPerson);
 
-router.post('/postman', (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res.status(400).json({ success: false, message: "Please provide a name" });
-    }
-    res.status(201).json({ success: true, data: [...people, name] });
-});
+router.post('/postman', postPersonPostman);
 
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const { name } = req.body;
+router.put('/:id', putPerson);
 
-    const person = people.find((person) => person.id === Number(id));
-
-    if (!person) {
-        return res.status(404).json({ success: false, message: `No person with id ${id} found.` });
-    }
-    const newPeople = people.map((person) => {
-        if (person.id === Number(id)) {
-            person.name = name;
-        }
-        return person;
-    })
-    res.status(200).json({ success: true, data: newPeople });
-});
-
-router.delete('/:id', (req, res) => {
-    const person = people.find((person) => person.id === Number(req.params.id));
-    if (!person) {
-        return res.status(404).json({ success: false, message: `No person with id ${req.params.id} found` });
-    }
-    const newPeople = people.filter((person) => person.id != Number(req.params.id));
-    return res.status(200).json({ success: true, data: newPeople });
-
-});
+router.delete('/:id', deletePerson);
 
 module.exports = router;
